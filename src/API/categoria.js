389d0f94cc84ab2f44da6d280ea9ee52977e3033
Cadu-Menezes <cadumenezes051@gmail.com>
2024-03-11 10:50:03 -0300

    Colocando validacao no campo

--- src/API/categoria.js
+++ src/API/categoria.js
@@ -6,25 +6,46 @@ const { body, check, validationResult } = require('express-validator');
 
 const categoriaService = new CategoriaService(categoria)
 
-//
+//Listando todas as categorias
 router.get('/lista', async(req, res) => {
+
     const categorias = await categoriaService.lista()
     res.send(categorias)
+
 })
 
-//exemplo de requisição post
-router.post("/cadastro", async(req, res) => {
+//Cadastrar uma nova categoria
+router.post("/cadastro", 
 
-    const {categoriadescricao, categoriaentrada, categoriadata} = req.body
+    //express-validador - Fazer validações nos campos
+    //Verifico se o campo categoriadescricao não está vazio e o minino de characters
+    body('categoriadescricao').not().isEmpty().trim().escape(), 
+    check('categoriadescricao').isLength({ min: 5 }).withMessage('Mínino de 5 dígitos'),
     
-    // body('categoriadescricao').not().isEmpty().trim().escape(),
-    // check('categoriadescricao').isLength({ min: 5 }).withMessage('Mínino de 5 dígitos'),
+    async(req, res) => {
     
+    //verificando se na req teve algum erro estourado pelo validador
+    const errors = validationResult(req)
+   
+    //caso sim retorno 400 com o erro q estourou
+    if (!errors.isEmpty()){ 
+   
+        return res.status(400).json({ errors: errors.array()})
+   
+    }
+
+    const {categoriadescricao, categoriaentrada, categoriadata} = req.body    
+        
     try{
+
         await categoriaService.cadastrar({categoriadescricao, categoriaentrada, categoriadata})
-        res.send('categoria adicionada com sucesso!')
-    }catch (erro){
-        res.send('Não foi possível inserir a categoria.')
+        res.status(200).send('categoria adicionada com sucesso!')
+    
+    }
+    catch (erro){
+    
+        res.status(400).send('Não foi possível inserir a categoria.')
+    
     }
 
 })
